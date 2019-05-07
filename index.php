d65fbca76952b07cd05aa520421faf5e3ece09f5
Alexander Topalo <saneock19@gmail.com>
2019-05-07 11:43:17 +0300

    dont display all errors

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-ini_set('error_reporting', E_ALL);
+ini_set('error_reporting', E_ALL - E_NOTICE - E_DEPRECATED - E_WARNING);
 ini_set('display_errors', 1);
 
 mb_internal_encoding("UTF-8");
